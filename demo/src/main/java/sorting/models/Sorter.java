aa0a3b90396c0b678082246830f2758863e664dc
abdelrahmanmohammednasr <abdoahlawy6161@gmail.com>
2025-03-06 03:08:16 +0200

    bubble sort

--- demo/src/main/java/sorting/models/Sorter.java
+++ demo/src/main/java/sorting/models/Sorter.java
@@ -1,5 +1,18 @@
 package sorting.models;
 
+import java.util.Vector;
+
 public class Sorter {
-    //implements sorting algorithms
+    void bubbleSort(Vector<Integer> arr)
+    {
+        int n = arr.size();
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr.get(j) > arr.get(j+1))
+                {
+                    int temp = arr.get(j);
+                    arr.set(j, arr.get(j+1));
+                    arr.set(j+1, temp);
+                }
+    }
 }
