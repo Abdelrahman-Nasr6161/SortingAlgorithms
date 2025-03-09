0a48163561e61f02f2807b972e67467cd2057470
abdelrahmanmohammednasr <abdoahlawy6161@gmail.com>
2025-03-09 13:03:26 +0200

    made sorting algorithms classes for better easier and decoupled implementations

--- demo/src/main/java/sorting/models/Sorter.java
+++ demo/src/main/java/sorting/models/Sorter.java
@@ -1,18 +1,15 @@
 package sorting.models;
 
-import java.util.Vector;
+import sorting.models.Sorts.Sort;
 
 public class Sorter {
-    void bubbleSort(Vector<Integer> arr)
-    {
-        int n = arr.size();
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr.get(j) > arr.get(j+1))
-                {
-                    int temp = arr.get(j);
-                    arr.set(j, arr.get(j+1));
-                    arr.set(j+1, temp);
-                }
+    private Sort sort;
+
+    public Sorter(Sort sort) {
+        this.sort = sort;
+    }
+
+    public void sort(int[] arr) {
+        sort.sort(arr);
     }
 }
---
+++ demo/src/main/java/sorting/models/Sorts/Sort.java
@@ -0,0 +1,6 @@
+package sorting.models.Sorts;
+
+public interface Sort {
+    public void sort(int[] arr);
+    public String getName();
+}
