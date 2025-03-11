14360d082915bc85656c23fa5e2bffa94dd4c64e
abdelrahmanmohammednasr <abdoahlawy6161@gmail.com>
2025-03-11 02:11:31 +0200

    ignore

--- demo/src/main/java/sorting/Main.java
+++ demo/src/main/java/sorting/Main.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 import sorting.models.Sorter;
 import sorting.models.Sorts.BubbleSort;
 import sorting.models.Sorts.InsertionSort;
+import sorting.models.Sorts.RadixSort;
 import sorting.models.Sorts.Sort;
 
 
-public class Main implements Runnable {
-    Sorter sorter;
-    public Main(Sort sort) { 
-        sorter = new Sorter(sort);
-        
-    }
-    public static void main(String[] args) {
+public class Main {
+    private static Sorter sorter;
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array");
         int n = sc.nextInt();
@@ -26,19 +23,33 @@ public class Main implements Runnable {
         System.out.println("Enter the sorting algorithm to be used");
         System.out.println("1. Bubble Sort");
         System.out.println("2. Insertion Sort");
+        System.out.println("3. Radix Sort");
         int choice = sc.nextInt();
         Sort sort = null;
-        if (choice == 1) {
+        switch (choice) {
+            case 1:
             sort = new BubbleSort();
-        } else if (choice == 2) {
+            break;
+            case 2:
             sort = new InsertionSort();
+            break;
+            case 3:
+            sort = new RadixSort();
+            break;
+            default:
+            System.out.println("Invalid choice");
+            System.exit(0);
         }
-        Main main = new Main(sort);
-        main.run();
-    }
-    @Override
-    public void run() {
-        
-        
+        sorter = new Sorter(sort);
+        int startTime = (int) System.nanoTime();
+        sorter.sort(arr);
+        int endTime = (int) System.nanoTime();
+        int elapsedTime = endTime - startTime;
+        System.out.println("Time taken: " + elapsedTime + " ns");
+        System.out.println("The array after sorting using " + sorter.getName() + " is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
     }
 }
--- demo/src/main/java/sorting/models/Sorts/RadixSort.java
+++ demo/src/main/java/sorting/models/Sorts/RadixSort.java
@@ -4,7 +4,7 @@ public class RadixSort implements Sort {
 
     @Override
     public void sort(int[] arr) {
-        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
